371e8ab9b8ed5e98a07fd7467b2092bd586a2b04
DaurenSan <sansizbai@gmail.com>
2018-09-18 11:09:36 +0600

    adding a map

--- src/body/body.js
+++ src/body/body.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
-import './body.css';
 import Header from './header/Header';
 import EmployeeList from './employeeList/employeeList';
-import MapContainer from './map';
-
 
 class Body extends Component {
   render() {
     return (
-        <div className="main">
-            <div className="sub">
-              <Header headerText="My fellas"/>
-              <EmployeeList />
-            </div>
-            <div className="sub">
-              <MapContainer />
-            </div>
+        <div>
+            <Header headerText="My fellas"/>
+            <EmployeeList />
         </div>
-
     );
   }
 }
